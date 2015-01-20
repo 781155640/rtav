9d45d1bf91e71d8b50d223579a421ceca88e926b
781155640 <781155640@qq.com>
2015-01-20 17:25:15 +0800

    add the decoder
    add the decoder

--- app/src/main/java/com/example/personall/rtav/Main.java
+++ app/src/main/java/com/example/personall/rtav/Main.java
@@ -1,22 +1,56 @@
 package com.example.personall.rtav;
 
 import android.app.Activity;
+import android.media.MediaCodec;
+import android.media.MediaExtractor;
+import android.media.MediaFormat;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.Socket;
+import java.nio.ByteBuffer;
 
 
 public class Main extends Activity {
+
+    private MediaCodec m_Codec;
+    private MediaExtractor m_Extractor;
+    private SurfaceView m_SurfaceView;
+    private Surface m_Surface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        m_SurfaceView=(SurfaceView)this.findViewById(R.id.surfaceView);
+
+        SurfaceHolder SurfaceHolder = m_SurfaceView.getHolder();
+        m_Surface = SurfaceHolder.getSurface();
+
+        int width=352;
+        int height=177;
+        MediaFormat format = m_Extractor.getTrackFormat(0);
+        String mime=format.getString(MediaFormat.KEY_MIME);
+        MediaFormat mediaFormat = MediaFormat.createVideoFormat(mime, width, height);
+
+        m_Codec = MediaCodec.createDecoderByType("Video/AVC");
+        m_Codec.configure(mediaFormat, m_Surface, null, 0);
+
+        if(m_Codec == null)
+        {
+            Log.e("Decodc","can't find video info");
+            return;
+        }
+
+        m_Codec.start();
 
         try {
             new Thread() {
@@ -33,8 +67,13 @@ public class Main extends Activity {
 
                     try {
                         sc = new Socket(ip, port);
+
                         netInputStream = new DataInputStream(sc.getInputStream());
                         netOutputStream = new DataOutputStream(sc.getOutputStream());
+
+
+                        ByteBuffer[] inputBuffers = m_Codec.getInputBuffers();
+                        ByteBuffer[] outputBuffers = m_Codec.getOutputBuffers();
 
                         /*
                         String msg = "hello, android";
@@ -47,32 +86,45 @@ public class Main extends Activity {
                         */
 
                         byte[] buffcontent = new byte[1204];
+                        int mCount=0;
+                        int length;
                         while(true)
                         {
                             int ct = netInputStream.read(buffcontent);
 
-                            System.out.println("read %");
+                            System.out.println("read "+ct+" bytes."+buffcontent[0]+":"+buffcontent[1]);
+
+                            length = ct;
+
+                            if (ct<0)
+                            {
+                                System.out.println("read error");
+                                break;
+                            }
+
+                            int inputBufferIndex = m_Codec.dequeueInputBuffer(-1);
+                            if (inputBufferIndex>=0){
+                                ByteBuffer inputBuffer = inputBuffers[inputBufferIndex];
+                                inputBuffer.clear();
+                                inputBuffer.put(buffcontent,0,length);
+                                m_Codec.queueInputBuffer(inputBufferIndex, 0, length, mCount * 1000000 / 15, 0);
+                                mCount++;
+                            }
+
+                            MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
+                            int outputBufferIndex = m_Codec.dequeueOutputBuffer(bufferInfo,0);
+                            while (outputBufferIndex >= 0) {
+                                m_Codec.releaseOutputBuffer(outputBufferIndex, true);
+                                outputBufferIndex = m_Codec.dequeueOutputBuffer(bufferInfo, 0);
+                            }
+
+                            System.out.println("read next frame");
                         }
                     }
                     catch(Exception e)
                     {
                         e.printStackTrace();
                     }
-
-
-
-                    Log.e("Decodc", "Thread is running.begin");
-                    try {
-                        Log.e("Decodc", "Thread is running.");
-                        System.out.println("Runnable running ");
-                        sleep(10);
-                    }
-                    catch (Exception e)
-                    {
-                        Log.e("Thread", "Thread error occour.");
-                        e.printStackTrace();
-                    }
-                    System.out.println("Thread Running end!");
                 }
             }.start();
         }
