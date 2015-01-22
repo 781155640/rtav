08b0130193a1e714c4e26d2cf95192befb5bc310
781155640 <781155640@qq.com>
2015-01-22 14:48:51 +0800

    try to get data from camera
    try to get data from camera

--- app/src/main/java/com/example/personall/rtav/Main.java
+++ app/src/main/java/com/example/personall/rtav/Main.java
@@ -2,6 +2,8 @@ package com.example.personall.rtav;
 
 import android.app.Activity;
 //*
+import android.graphics.ImageFormat;
+import android.hardware.Camera;
 import android.media.MediaCodec;
 import android.media.MediaCodecInfo;
 import android.media.MediaExtractor;
@@ -30,6 +32,8 @@ public class Main extends Activity {
     private SurfaceView m_SurfaceView;
     private Surface m_Surface;
     private SurfaceHolder m_SurfaceHolder;
+
+    private Camera cam;
     //*/
 
     @Override
@@ -50,133 +54,24 @@ public class Main extends Activity {
                         public void run()
                         {
                             Log.e("Decodc", "Thread is running.2");
+                            m_Surface = SurfaceHolder.getSurface();
 
-                            /*
-                            MediaFormat mediaFormat = MediaFormat.createVideoFormat("video/avc",352, 288);
-                            mediaFormat.setInteger(MediaFormat.KEY_BIT_RATE, 125000);
-                            mediaFormat.setInteger(MediaFormat.KEY_FRAME_RATE, 24);
-                            mediaFormat.setInteger(MediaFormat.KEY_CHANNEL_COUNT, 1);
-                            //mediaFormat.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420Planar);
-                            mediaFormat.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420SemiPlanar);
-                            //mediaFormat.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420PackedPlanar);
-                            mediaFormat.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, 5);
-                            */
+
                             //*
-                            Log.e("decoder","decode wile begin_3");
-                        try {
-                            m_Extractor  = new MediaExtractor();
-                            String fileString="/storage/sdcard/1.264";
-                            m_Extractor.setDataSource(fileString);
-
-                            Log.e("CODEC","count:"+m_Extractor.getTrackCount());
-
-                            return;
-
-/*
-                            for (int i = 0; i < m_Extractor.getTrackCount(); i++) {
-
-                                MediaFormat format = m_Extractor.getTrackFormat(i);
-                                String mime = format.getString(MediaFormat.KEY_MIME);
-                                if (mime.startsWith("video/")) {
-                                    m_Extractor.selectTrack(i);
-                                    m_Codec = MediaCodec.createDecoderByType(mime);
-                                    m_Codec.configure(format, m_Surface, null, 0);
-                                    break;
-                                }
-
-                            }
-*/
-
-                            /*
-                            m_Codec = MediaCodec.createDecoderByType("video/avc");
-                            Log.e("decoder","decode wile begin_4");
-                            m_Codec.configure(mediaFormat, m_Surface, null, 0);
-                            */
-                            /*
-                            if(m_Codec == null)
-                            {
-                                Log.e("Decodc","can't find video info");
-                                return;
-                            }
-
-                            Log.e("decoder","decode wile begin_1");
-                            m_Codec.start();
-                            //*/
-
-                            /*
-                            Socket sc;
-                            DataInputStream netInputStream;
-                            DataOutputStream netOutputStream;
-
-                            //String ip = "192.168.1.236";
-                            String ip = "192.168.0.61";
-                            int port = 3000;
-
-                            Log.e("decoder","decode wile begin_2");
-
-
-
-
-
-
-                                sc = new Socket(ip, port);
-
-                                netInputStream = new DataInputStream(sc.getInputStream());
-                                netOutputStream = new DataOutputStream(sc.getOutputStream());
-*/
-                                /*
-                                ByteBuffer[] inputBuffers = m_Codec.getInputBuffers();
-                                ByteBuffer[] outputBuffers = m_Codec.getOutputBuffers();
-                                //*/
-/*
-                                byte[] buffcontent = new byte[100*1204];
-
-                                int mCount=0;
-                                int length;
-
-                                while(true)
-                                {
-                                    //int ct = netInputStream.read(buffcontent);
-
-                                    System.out.println("__read "+1+" bytes."+buffcontent[0]+":"+buffcontent[1]);
-
-                                    //length = ct;
-
-                                    int inputBufferIndex = m_Codec.dequeueInputBuffer(-1);
-                                    if (inputBufferIndex>=0){
-                                        ByteBuffer inputBuffer = inputBuffers[inputBufferIndex];
-
-                                        int sampleSize = m_Extractor.readSampleData(inputBuffer, 0);
-
-                                        m_Codec.queueInputBuffer(inputBufferIndex, 0, sampleSize,
-                                                m_Extractor.getSampleTime(), 0);
-                                        m_Extractor.advance();
-
-
-                                        System.out.println("__read: "+sampleSize+" bytes."+buffcontent[0]+":"+buffcontent[1]);
-
-                                        inputBuffer.clear();
-                                        inputBuffer.put(buffcontent,0,length);
-                                        m_Codec.queueInputBuffer(inputBufferIndex, 0, length, mCount * 1000000 / 25, 0);
-                                        mCount++;
-
-                                    }
-
-                                    MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
-                                    int outputBufferIndex = m_Codec.dequeueOutputBuffer(bufferInfo,0);
-                                    if (outputBufferIndex >= 0) {
-                                        m_Codec.releaseOutputBuffer(outputBufferIndex, true);
-                                        outputBufferIndex = m_Codec.dequeueOutputBuffer(bufferInfo, 0);
-                                    }
-
-                                    System.out.println("read next frame");
-                                }
-                        //*/
-                            }
-                            catch(Exception e)
-                            {
-                                e.printStackTrace();
-                            }
+                            cam = Camera.open();
+                            cam.setPreviewDisplay(SurfaceHolder);
+                            Camera.Parameters parameters = cam.getParameters();
+                            parameters.setFlashMode("off"); // 无闪光灯
+                            parameters.setWhiteBalance(Camera.Parameters.WHITE_BALANCE_AUTO);
+                            parameters.setSceneMode(Camera.Parameters.SCENE_MODE_AUTO);
+                            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+                            parameters.setPreviewFormat(ImageFormat.YV12);
+                            int camWidth=352;
+                            int camHeight=177;
+                            parameters.setPictureSize(camWidth, camHeight);
+                            parameters.setPreviewSize(camWidth, camHeight);
+                            //这两个属性 如果这两个属性设置的和真实手机的不一样时，就会报错
+                            cam.setParameters(parameters);
                         }
                     }.start();
                 }
@@ -199,13 +94,6 @@ public class Main extends Activity {
             }
         });
         m_Surface = m_SurfaceHolder.getSurface();
-
-
-        //m_SurfaceHolder.setOnPreparedListener();
-        //m_Surface.setOnPreparedListener();
-
-        //*/
-
 
     }
 
